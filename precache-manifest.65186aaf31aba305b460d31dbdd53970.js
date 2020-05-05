8f3e26a8a9a1875d218323eacc2d562a5fe40a15
rickybeeloco <reekardoe@yahoo.com>
2020-05-05 15:20:55 -0700

    Updates

---
+++ precache-manifest.65186aaf31aba305b460d31dbdd53970.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db3a5a141b16804481e1eba36bb45b9",
+    "url": "/shoot-the-moon-rick.git/index.html"
+  },
+  {
+    "revision": "3bf7535f801ffc3a9a0e",
+    "url": "/shoot-the-moon-rick.git/static/js/2.7c64cff7.chunk.js"
+  },
+  {
+    "revision": "a279015ada6827c525c898fa55497f39",
+    "url": "/shoot-the-moon-rick.git/static/js/2.7c64cff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "583b5df6d9b502cb82d3",
+    "url": "/shoot-the-moon-rick.git/static/js/main.35a27d00.chunk.js"
+  },
+  {
+    "revision": "cd57c7c883a59ac1c64b",
+    "url": "/shoot-the-moon-rick.git/static/js/runtime-main.638824b1.js"
+  },
+  {
+    "revision": "07d5ba4a894c77d6f799ecde7e84850f",
+    "url": "/shoot-the-moon-rick.git/static/media/cart.07d5ba4a.svg"
+  },
+  {
+    "revision": "a8a3ba3a10003218c2a017cf68d20489",
+    "url": "/shoot-the-moon-rick.git/static/media/curve.a8a3ba3a.svg"
+  },
+  {
+    "revision": "f25b4cba48a496093085f268698a4b39",
+    "url": "/shoot-the-moon-rick.git/static/media/dog-1.f25b4cba.png"
+  },
+  {
+    "revision": "ab7a533bb445ea0d167a34cdc0eec387",
+    "url": "/shoot-the-moon-rick.git/static/media/dog-2.ab7a533b.png"
+  },
+  {
+    "revision": "b62803749fd80890a425b0dfae080c47",
+    "url": "/shoot-the-moon-rick.git/static/media/dog-3.b6280374.png"
+  },
+  {
+    "revision": "98f2a8b1296f385203e7b9b5d9938fcf",
+    "url": "/shoot-the-moon-rick.git/static/media/exit.98f2a8b1.svg"
+  },
+  {
+    "revision": "0b1c67758d564d5eb140740847e6886e",
+    "url": "/shoot-the-moon-rick.git/static/media/galaxy.0b1c6775.jpg"
+  },
+  {
+    "revision": "3192f86cb3a13b08200f5e3d3fef0325",
+    "url": "/shoot-the-moon-rick.git/static/media/home.3192f86c.svg"
+  },
+  {
+    "revision": "48932214f494cf346794f464ee2c04e3",
+    "url": "/shoot-the-moon-rick.git/static/media/iss.48932214.jpg"
+  },
+  {
+    "revision": "0c7bf061e88c942e5031e971c6ab447b",
+    "url": "/shoot-the-moon-rick.git/static/media/logo.0c7bf061.svg"
+  },
+  {
+    "revision": "493bbb752333aacb0e80bab31c2e925a",
+    "url": "/shoot-the-moon-rick.git/static/media/moon.493bbb75.jpg"
+  },
+  {
+    "revision": "ea589b574fff44db577387da01243704",
+    "url": "/shoot-the-moon-rick.git/static/media/profile.ea589b57.svg"
+  },
+  {
+    "revision": "c27cc1f53fed31cbbc1d743f9bbaba1d",
+    "url": "/shoot-the-moon-rick.git/static/media/rocket.c27cc1f5.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/shoot-the-moon-rick.git/static/media/space.7dfc7923.jpg"
+  }
+]);
